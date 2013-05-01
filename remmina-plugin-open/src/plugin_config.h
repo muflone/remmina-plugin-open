f11bf05a4bcf55f92278d2d9ef6f6f9498e396c4
Muflone <muflone@vbsimple.net>
2013-05-01 15:14:40 +0200

    First code commit

---
+++ remmina-plugin-open/src/plugin_config.h
@@ -0,0 +1,8 @@
+#ifndef __PLUGIN_CONFIG_H
+#define __PLUGIN_CONFIG_H
+
+#define PLUGIN_NAME        "OPEN"
+#define PLUGIN_DESCRIPTION "OPEN - Open a document with its application"
+#define PLUGIN_VERSION     "1.0.0.0"
+#define PLUGIN_APPICON     "remmina-open"
+#endif
