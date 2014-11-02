f30ef1272c6eb1ad25fe65f2a0e485a32bf4356e
Muflone <webreg@vbsimple.net>
2014-11-02 13:34:20 +0100

    Updated for GTK+ 3

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-Project name : Remmina Plugin OPEN
-Remmina protocol plugin to open a document with its associated application.
+    Project: Remmina Plugin OPEN
+Description: Remmina protocol plugin to open a document with its associated
+             application.
+     Author: Fabio Castelli (Muflone) <muflone@vbsimple.net>
+  Copyright: 2011-2014 Fabio Castelli (Muflone)
+    License: GPL-2+
 
-Copyright (C) 2011-2013 Fabio Castelli <muflone@vbsimple.net>
-
-This program is free software: you can redistribute it and/or modify
+This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
+the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- remmina-plugin-open/src/plugin_config.h
+++ remmina-plugin-open/src/plugin_config.h
@@ -1,8 +1,30 @@
-#ifndef __PLUGIN_CONFIG_H
-#define __PLUGIN_CONFIG_H
+/*
+ *     Project: Remmina Plugin OPEN
+ * Description: Remmina protocol plugin to open a document with its associated
+ *              application.
+ *      Author: Fabio Castelli (Muflone) <muflone@vbsimple.net>
+ *   Copyright: 2011-2014 Fabio Castelli (Muflone)
+ *     License: GPL-2+
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of ERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#define PLUGIN_NAME        "OPEN"
-#define PLUGIN_DESCRIPTION "OPEN - Open a document with its application"
-#define PLUGIN_VERSION     "1.0.0.1"
-#define PLUGIN_APPICON     "remmina-open"
+#ifndef __PLUGIN_CONFIG_H
+  #define __PLUGIN_CONFIG_H
+
+  #define PLUGIN_NAME        "OPEN"
+  #define PLUGIN_DESCRIPTION "OPEN - Open a document with its application"
+  #define PLUGIN_VERSION     "1.1.1.0"
+  #define PLUGIN_APPICON     "remmina-open"
 #endif
