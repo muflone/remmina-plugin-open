5e255114b2954aac25bbca2902f812856730a0b9
Muflone <webreg@vbsimple.net>
2015-11-15 00:42:40 +0100

    Renamed functions

--- remmina-plugin-open/src/remmina_plugin.c
+++ remmina-plugin-open/src/remmina_plugin.c
@@ -35,14 +35,14 @@ static gpointer launchers_list[] =
 
 static RemminaPluginService *remmina_plugin_service = NULL;
 
-static void remmina_plugin_init(RemminaProtocolWidget *gp)
+static void remmina_plugin_open_init(RemminaProtocolWidget *gp)
 {
-  remmina_plugin_service->log_printf("[%s] remmina_plugin_init\n", PLUGIN_NAME);
+  remmina_plugin_service->log_printf("[%s] remmina_plugin_open_init\n", PLUGIN_NAME);
 }
 
-static gboolean remmina_plugin_open_connection(RemminaProtocolWidget *gp)
+static gboolean remmina_plugin_open_open_connection(RemminaProtocolWidget *gp)
 {
-  remmina_plugin_service->log_printf("[%s] remmina_plugin_open_connection\n", PLUGIN_NAME);
+  remmina_plugin_service->log_printf("[%s] remmina_plugin_open_open_connection\n", PLUGIN_NAME);
   #define GET_PLUGIN_STRING(value) \
     g_strdup(remmina_plugin_service->file_get_string(remminafile, value))
   #define GET_PLUGIN_BOOLEAN(value) \
@@ -79,14 +79,14 @@ static gboolean remmina_plugin_open_connection(RemminaProtocolWidget *gp)
   return FALSE;
 }
 
-static gboolean remmina_plugin_close_connection(RemminaProtocolWidget *gp)
+static gboolean remmina_plugin_open_close_connection(RemminaProtocolWidget *gp)
 {
-  remmina_plugin_service->log_printf("[%s] remmina_plugin_close_connection\n", PLUGIN_NAME);
+  remmina_plugin_service->log_printf("[%s] remmina_plugin_open_close_connection\n", PLUGIN_NAME);
   remmina_plugin_service->protocol_plugin_emit_signal(gp, "disconnect");
   return FALSE;
 }
 
-static const RemminaProtocolSetting remmina_plugin_basic_settings[] =
+static const RemminaProtocolSetting remmina_plugin_open_basic_settings[] =
 {
   { REMMINA_PROTOCOL_SETTING_TYPE_TEXT, "server", N_("Startup program"), FALSE, NULL, NULL },
   { REMMINA_PROTOCOL_SETTING_TYPE_SELECT, "launcher", N_("Launcher"), FALSE, launchers_list, NULL },
@@ -102,13 +102,13 @@ static RemminaProtocolPlugin remmina_plugin =
   PLUGIN_VERSION,
   PLUGIN_APPICON,
   PLUGIN_APPICON,
-  remmina_plugin_basic_settings,
+  remmina_plugin_open_basic_settings,
   NULL,
   REMMINA_PROTOCOL_SSH_SETTING_NONE,
   NULL,
-  remmina_plugin_init,
-  remmina_plugin_open_connection,
-  remmina_plugin_close_connection,
+  remmina_plugin_open_init,
+  remmina_plugin_open_open_connection,
+  remmina_plugin_open_close_connection,
   NULL,
   NULL
 };
