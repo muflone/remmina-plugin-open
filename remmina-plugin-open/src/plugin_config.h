0a775ce2ca4d25bcb61cf0f97fe22364125eb1c4
Muflone <webreg@vbsimple.net>
2015-11-15 21:00:37 +0100

    Bumped version number

--- remmina-plugin-open/src/plugin_config.h
+++ remmina-plugin-open/src/plugin_config.h
@@ -25,6 +25,6 @@
 
   #define PLUGIN_NAME        "OPEN"
   #define PLUGIN_DESCRIPTION "OPEN - Open a document with its application"
-  #define PLUGIN_VERSION     "1.2.1.0"
+  #define PLUGIN_VERSION     "1.2.2.0"
   #define PLUGIN_APPICON     "remmina-open"
 #endif
